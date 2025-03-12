e316a2d41f5b3bee8b9668e13544d74362ac0306
Vijay Shankar <vijay.shankar@msci.com>
2025-03-12 20:15:02 +0530

    first commit

---
+++ Clipboard/ClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardApp.swift
+//  Clipboard
+//
+//  Created by vijay shankar on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipboardTests/ClipboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardTests.swift
+//  ClipboardTests
+//
+//  Created by vijay shankar on 12/03/25.
+//
+
+import Testing
+@testable import Clipboard
+
+struct ClipboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
